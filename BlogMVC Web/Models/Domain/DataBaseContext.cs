7a54d7f29014cbd74e6c47b13fde31000292523d
louis <quocbinhgithub@gmail.com>
2023-06-18 15:41:24 +0700

    add headerUI bootstrap

---
+++ BlogMVC Web/Models/Domain/DataBaseContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BlogMVC_Web.Models.Domain
+{
+    public class DataBaseContext : DbContext
+    {
+        public DataBaseContext(DbContextOptions<DataBaseContext> options) : base()
+        {
+
+        }
+
+        public DbSet<Person> Person { get; set; }
+    }
+}
