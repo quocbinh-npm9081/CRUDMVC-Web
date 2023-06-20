9e9148ca6b224cb67e4dc0b0b7bd5fdcac0c145d
louis <quocbinhgithub@gmail.com>
2023-06-20 09:45:45 +0700

    add edit delete feature

--- BlogMVC Web/Controllers/PersonController.cs
+++ BlogMVC Web/Controllers/PersonController.cs
@@ -1,5 +1,6 @@
 ﻿using BlogMVC_Web.Models.Domain;
 using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Linq.Expressions;
 
 namespace BlogMVC_Web.Controllers
@@ -7,11 +8,10 @@ namespace BlogMVC_Web.Controllers
     public class PersonController : Controller
     {
         private readonly DataBaseContext _context;
-
-        public PersonController(DataBaseContext context) {
+        public PersonController(DataBaseContext context)
+        {
             _context = context;
         }
-
         public IActionResult Index()
         {
             //ways passing data to view
@@ -22,20 +22,19 @@ namespace BlogMVC_Web.Controllers
             TempData["greeting2"] = "Its Tempalte msg";
             return View();
         }
-
         public IActionResult DisplayPersons()
         {
             var person = _context.Person.ToList();
             return View(person);
         }
-
         public IActionResult AddPerson()
         {
             return View();
         }
         [HttpPost]
-        public IActionResult AddPerson(Person person) {
-            if(!ModelState.IsValid)
+        public IActionResult AddPerson(Person person)
+        {
+            if (!ModelState.IsValid)
             {
                 return View();
             }
@@ -52,6 +51,45 @@ namespace BlogMVC_Web.Controllers
                 TempData["submit_msg"] = "Add failse";
                 return View();
             }
+        }
+        public IActionResult EditPerson(int id)
+        {
+            var person = _context.Person.Find(id);
+            return View(person);
+        }
+        [HttpPost]
+        public IActionResult EditPerson(Person person)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+
+            if (person != null)
+            {
+                _context.Person.Update(person);
+                _context.SaveChanges();
+            }
+
+            return RedirectToAction("DisplayPersons");
+
+        }
+        public IActionResult DeletePerson(int id)
+        {
+            try
+            {
+                var person = _context.Person.Find(id);
+                if (person != null)
+                {
+                    _context.Person.Remove(person);
+                    _context.SaveChanges();
+                }
+            }
+            catch (Exception ex)
+            {
+                TempData["submit_msg"] = "Delete failse";
+            }
+            return RedirectToAction("DisplayPersons");
         }
     }
 }
